c031cd5052c6a61249e8d9b9410d6fedbc6838b3
FrankBosse <francis.bosse@edu.sait.ca>
2024-01-23 14:45:08 -0700

    Finished Assignment 2
    made new page called week-2, edited the root page to link to second page

---
+++ app/nav-assignments.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function NavAssignments() {
+  return (
+    <nav className="text-yellow-400">
+      <Link href="/week-2">Week 2 Assignment</Link>
+    </nav>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <nav>
+      <h1>Frank Bosse</h1>
+      <Link href="https://github.com/FrankBosse/cprg306-assignments">
+        My GitHub
+      </Link>
+    </nav>
+  );
+}
